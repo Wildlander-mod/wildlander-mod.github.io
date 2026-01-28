05a50aa59a326aed0a9fd33dbfb953d57f478f97
Mieekaserra <pzzaz_st@yahoo.co.uk>
2026-01-28 14:05:45 +0000

    modlist

--- _includes/AirtableConversionScripts/Convert-ModlistCSV.ps1
+++ _includes/AirtableConversionScripts/Convert-ModlistCSV.ps1
@@ -3,20 +3,25 @@
 
 $csv = Import-Csv "DataFromAirtable\Current Mod list.csv"
 $output = @()
-$output += "| Group | MO2 Name | Version | Nexus Mod Name | Category | Author | Nexus URL |"
-$output += "|-------|----------|---------|----------------|----------|--------|-----------|"
+$output += "| Group | MO2 Name | Lookup URL | Version | Nexus URL | Nexus Mod Name | Category | Author | Author URL | Kofi | Paypal | Patreon |"
+$output += "|-------|----------|-----------|---------|-----------|----------------|----------|--------|-----------|------|--------|---------|"
 
 foreach ($row in $csv) {
   $group = $row.'Group' -replace '\|', '\|'
   $mo2Name = ($row.'Mo2 Name' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
+  $lookupUrl = ($row.'Lookup URL' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
   $version = $row.'Version' -replace '\|', '\|'
+  $nexusUrl = $row.'Nexus URL' -replace '\|', '\|'
   $nexusModName = ($row.'Nexus Mod Name' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
   $category = $row.'Category' -replace '\|', '\|'
   $author = ($row.'Author' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
-  $nexusUrl = $row.'Nexus URL' -replace '\|', '\|'
+  $authorUrl = ($row.'Author URL' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
+  $kofi = ($row.'Kofi' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
+  $paypal = ($row.'Paypal' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
+  $patreon = ($row.'Patreon' -replace '\n', ' ' -replace '\r', ' ' -replace '\|', '\|')
   
   if ($mo2Name) {
-    $output += "| $group | $mo2Name | $version | $nexusModName | $category | $author | $nexusUrl |"
+    $output += "| $group | $mo2Name | $lookupUrl | $version | $nexusUrl | $nexusModName | $category | $author | $authorUrl | $kofi | $paypal | $patreon |"
   }
 }
 
